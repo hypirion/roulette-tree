3ee551dd3fc3fabc74e44ce4d56f30d1a45511bd
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-04-20 13:27:43 +0200

    Specify array-roulette implementation header.

---
+++ include/array_roulette.h
@@ -0,0 +1,24 @@
+#ifndef ARRAY_ROULETTE_H
+#define ARRAY_ROULETTE_H
+#include <stdint.h>
+
+typedef struct {
+  void *elt_ptr;
+  double fit_from, fit_to;
+} aroulette_elt;
+
+typedef struct {
+  aroulette_elt *arr;
+  uint32_t len, cap;
+} aroulette_t;
+
+aroulette_t *aroulette_create();
+void aroulette_destroy(aroulette_t *ar);
+
+void aroulette_add(aroulette_t *ar, void *elt_ptr, double fitness);
+
+void *aroulette_rget(aroulette_t *ar);
+void *aroulette_rpop(aroulette_t *ar);
+
+uint32_t aroulette_length(aroulette_t *ar);
+#endif
