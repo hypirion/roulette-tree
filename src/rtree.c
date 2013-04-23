a14a6e7e6e018f11d8de3664068684df2d45ba2e
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-04-23 22:23:43 +0200

    Define rotation fns.

--- src/rtree.c
+++ src/rtree.c
@@ -3,11 +3,7 @@
 #include <stdint.h>
 #include "rtree.h"
 
-rtree_t *rtree_create() {
-  rtree_t *rt = malloc(sizeof(rtree_t));
-  rt->root = NULL;
-  return rt;
-}
+// Private functions, used inside the structure.
 
 static void dfs_destroy(rtree_node_t *rnode) {
   if (rnode == NULL) {
@@ -16,6 +12,35 @@ static void dfs_destroy(rtree_node_t *rnode) {
   dfs_destroy(rnode->link[0]);
   dfs_destroy(rnode->link[1]);
   free(rnode);
+}
+
+static bool is_red(rtree_node_t *rt) {
+  return rt != NULL && rt->red == true;
+}
+
+static rtree_node_t *rot_once(rtree_node_t *root, int dir) {
+  rtree_node_t *ret = root->link[!dir];
+
+  root->link[!dir] = ret->link[dir];
+  ret->link[dir] = root;
+
+  root->red = true;
+  ret->red = false;
+
+  return ret;
+}
+
+static rtree_node_t *rot_twice(rtree_node_t *root, int dir) {
+  root->link[!dir] = rot_once(root->link[!dir], !dir);
+  return rot_once(root, dir);
+}
+
+// Public functions, available for everyone to use.
+
+rtree_t *rtree_create() {
+  rtree_t *rt = malloc(sizeof(rtree_t));
+  rt->root = NULL;
+  return rt;
 }
 
 void rtree_destroy(rtree_t *rt) {
