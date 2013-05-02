24c5dae899c6ce491075c8bd17d53898d8cd83d4
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-05-02 11:37:18 +0200

    Test removal (break everything).

--- src/main.c
+++ src/main.c
@@ -10,6 +10,15 @@ int main(int argc, char **argv) {
     rtree_preorder(rt);
     printf("\n");
   }
+  printf("Now removing items...\n\n");
+  for (int i = 0; i < 8; i++) {
+    long l = (long) rtree_rpop(rt);
+    printf("Got back %ld\n", l);
+    rb_check(rt->root);
+    total_fit_check(rt);
+    rtree_preorder(rt);
+    printf("\n");
+  }
   rtree_destroy(rt);
   return 0;
 }
