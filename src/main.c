665eb50aae1250da9952e38ecfe59967e8b83a64
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-05-03 21:10:31 +0200

    Attempt to debug and understand the situation.

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv) {
   rtree_t *rt = rtree_create();
   for (int i = 0; i < 8; i++) {
-    rtree_add(rt, NULL, (double) (i+1));
+    rtree_add(rt, (void *) ((long) (i + 1)), (double) (i+1));
     rb_check(rt->root);
     total_fit_check(rt);
     rtree_preorder(rt);
@@ -12,11 +12,14 @@ int main(int argc, char **argv) {
   }
   printf("Now removing items...\n\n");
   for (int i = 0; i < 8; i++) {
+    char outfile[80];
+    sprintf(outfile, "imgs/test-%.2d.dot", i);
+    rtree_to_dot(rt, outfile);
     long l = (long) rtree_rpop(rt);
     printf("Got back %ld\n", l);
     rb_check(rt->root);
     total_fit_check(rt);
-    rtree_preorder(rt);
+    //    rtree_preorder(rt);
     printf("\n");
   }
   rtree_destroy(rt);
