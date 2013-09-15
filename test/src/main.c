83aedc4b12eeda0e6e0e363ad36acdf7287200fe
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-09-15 16:19:29 +0200

    Add in fitness check test.

--- test/src/main.c
+++ test/src/main.c
@@ -8,6 +8,7 @@
 int tests_run = 0;
 
 static char *test_all_kept() {
+  puts("Testing that inserted elements doesn't disappear or duplicate.");
   for (int count = 0; count < 1000; count++) {
     rtree_t *rt = rtree_create();
     for (uintptr_t i = 0; i < count; i++) {
@@ -31,8 +32,34 @@ static char *test_all_kept() {
   return 0;
 }
 
+static char *test_consistent_fitness() {
+  puts("Testing that the fitness of a randomly mutated rtree is consistent.");
+  for (int count = 0; count < 1000; count++) {
+    rtree_t *rt = rtree_create();
+    uint32_t rounds = (uint32_t) lrand48() % 50;
+    while (rounds --> 0) {
+      uint32_t insertions = (uint32_t) lrand48() % 50;
+      while (insertions --> 0) {
+        rtree_add(rt, (void *) NULL, drand48() * 15.0);
+        mu_assert("Total fitness doesn't add up", 0.0 <= total_fit_check(rt));
+      }
+      uint32_t deletions = 0;
+      if (rtree_size(rt) > 0) { // Avoid division by zero
+        deletions = ((uint32_t) lrand48()) % rtree_size(rt);
+      }
+      while (deletions --> 0) {
+        rtree_rpop(rt);
+        mu_assert("Total fitness doesn't add up", 0.0 <= total_fit_check(rt));
+      }
+    }
+    rtree_destroy(rt);
+  }
+  return 0;
+}
+
 static char *all_tests() {
   mu_run_test(test_all_kept);
+  mu_run_test(test_consistent_fitness);
   return 0;
 }
 
