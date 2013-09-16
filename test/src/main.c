63c868252299d27e37233996b0ced8a3a026ff80
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-09-16 12:16:03 +0200

    Test duplicate allowance.

--- test/src/main.c
+++ test/src/main.c
@@ -5,6 +5,13 @@
 #include "minunit.h"
 #include "main.h"
 
+#define __GENSYM2(x,y) x##y
+#define __GENSYM1(x,y) __GENSYM2(x,y)
+#define GENSYM(x) __GENSYM1(x,__COUNTER__)
+
+#define __REPEAT(name, n) for (int name = 0; name < n; name++)
+#define REPEAT(n) __REPEAT(GENSYM(iter), n)
+
 int tests_run = 0;
 
 static char *test_all_kept() {
@@ -32,14 +39,39 @@ static char *test_all_kept() {
   return 0;
 }
 
+static char *test_duplicates() {
+  puts("Testing that duplicates are allowed.");
+  rtree_t *rt = rtree_create();
+  const int elts = 10;
+  const int nof_insertions = 10;
+  for (uintptr_t elt = 0; elt < elts; elt++) {
+    REPEAT (nof_insertions) {
+      rtree_add(rt, (void *) elt, 1.0);
+    }
+  }
+  int *count = calloc(10, sizeof(int));
+  while (rtree_size(rt) > 0) {
+    count[(uintptr_t) rtree_rpop(rt)]++;
+  }
+  for (uintptr_t elt = 0; elt < elts; elt++) {
+    char *msg = calloc(80, sizeof(char));
+    sprintf(msg, "Expected %d elements for %ld, not %d.\n", nof_insertions, elt,
+            count[elt]);
+    mu_assert(msg, count[elt] == nof_insertions);
+    free(msg);
+  }
+  free(count);
+  return 0;
+}
+
 static char *test_consistent_fitness() {
   puts("Testing that the fitness of a randomly mutated rtree is consistent.");
   for (int count = 0; count < 1000; count++) {
     rtree_t *rt = rtree_create();
     uint32_t rounds = (uint32_t) lrand48() % 50;
-    while (rounds --> 0) {
+    REPEAT (rounds) {
       uint32_t insertions = (uint32_t) lrand48() % 50;
-      while (insertions --> 0) {
+      REPEAT (insertions) {
         rtree_add(rt, (void *) NULL, drand48() * 15.0);
         mu_assert("Total fitness doesn't add up", 0.0 <= total_fit_check(rt));
       }
@@ -47,7 +79,7 @@ static char *test_consistent_fitness() {
       if (rtree_size(rt) > 0) { // Avoid division by zero
         deletions = ((uint32_t) lrand48()) % rtree_size(rt);
       }
-      while (deletions --> 0) {
+      REPEAT (deletions) {
         rtree_rpop(rt);
         mu_assert("Total fitness doesn't add up", 0.0 <= total_fit_check(rt));
       }
@@ -62,9 +94,9 @@ static char *test_rb_invariants() {
   for (int count = 0; count < 1000; count++) {
     rtree_t *rt = rtree_create();
     uint32_t rounds = (uint32_t) lrand48() % 50;
-    while (rounds --> 0) {
+    REPEAT (rounds) {
       uint32_t insertions = (uint32_t) lrand48() % 50;
-      while (insertions --> 0) {
+      REPEAT (insertions) {
         rtree_add(rt, (void *) NULL, drand48() * 15.0);
         mu_assert("Red-black violation.", rb_check(rt->root) > 0);
       }
@@ -72,7 +104,7 @@ static char *test_rb_invariants() {
       if (rtree_size(rt) > 0) { // Avoid division by zero
         deletions = ((uint32_t) lrand48()) % rtree_size(rt);
       }
-      while (deletions --> 0) {
+      REPEAT (deletions) {
         rtree_rpop(rt);
         mu_assert("Red-black violation.", rb_check(rt->root) > 0);
       }
@@ -131,6 +163,7 @@ static char *test_uniform_frequency() {
 
 static char *all_tests() {
   mu_run_test(test_all_kept);
+  mu_run_test(test_duplicates);
   mu_run_test(test_consistent_fitness);
   mu_run_test(test_rb_invariants);
   mu_run_test(test_nonuniform_frequency);
