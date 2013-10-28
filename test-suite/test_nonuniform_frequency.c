a142d2ab113836626686e9023d61693aacbd3e64
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-10-28 10:15:39 +0100

    Split out nonuniform frequency test.

--- test-suite/test_nonuniform_frequency.c
+++ test-suite/test_nonuniform_frequency.c
@@ -25,59 +25,28 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "rtree.h"
-#include "minunit.h"
-#include "main.h"
+#include "test.h"
 
-#define __GENSYM2(x,y) x##y
-#define __GENSYM1(x,y) __GENSYM2(x,y)
-#define GENSYM(x) __GENSYM1(x,__COUNTER__)
-
-#define FOR(name, n) for (int name = 0; name < n; name++)
-#define REPEAT(n) FOR(GENSYM(iter), n)
-
-int tests_run = 0;
-int tests_failed = 0;
-
-
-static int test_uniform_frequency() {
-  puts("Testing uniform frequency.");
-  RTree *rt = rtree_create();
-  const int elems = 100;
-  for (uintptr_t i = 0; i < elems; i++) {
-    rtree_add(rt, (void *) i, 1.0);
-  }
-  const int size = 100000;
-  const double prob = 1.0/((double) elems);
-  int *count = calloc(elems, sizeof(int));
-  for (int i = 0; i < size; i++) {
-    count[(uintptr_t) rtree_rget(rt)]++;
-  }
-  for (int i = 0; i < elems; i++) {
-    double freq = ((double) count[i])/size;
-    if (!(prob - 0.005 < freq && freq < prob + 0.005)) {
-      printf("[FAIL] Expected frequency for %d to be within %.4f and %.4f, was "
-             "%.4f.\n", i, prob - 0.005, prob + 0.005, freq);
-      return 1;
-    }
-  }
-  free(count);
-  rtree_destroy(rt);
-  return 0;
-}
-
-static void all_tests() {
-  mu_run_test(test_uniform_frequency);
-}
+#define ITERATIONS 1000
 
 int main() {
-  all_tests();
-  if (tests_failed) {
-    printf("%d test%s failed.\n", tests_failed, tests_failed == 1 ? "" : "s");
+  puts("Testing nonuniform frequency.");
+  int tests_failed = 0;
+  RTree *rt = rtree_create();
+  rtree_add(rt, (void *) ((uintptr_t) 0), 1.0);
+  rtree_add(rt, (void *) ((uintptr_t) 1), 9.0);
+  const int picks = 10000;
+  int count[2] = {0, 0};
+  for (int i = 0; i < picks; i++) {
+    count[(uintptr_t) rtree_rget(rt)]++;
   }
-  else {
-    printf("All tests passed.\n");
+  // printf("Frequency for 0.1 value: %.4f\n", ((double) count[0])/picks);
+  // printf("Frequency for 0.9 value: %.4f\n", ((double) count[1])/picks);
+  double val = ((double) count[0])/picks;
+  // TODO: 99% confidence intervals? need something more sane here.
+  if (!(0.1 - 0.01 < val && val < 0.1 + 0.01)) {
+    puts("Frequency is too improbable.");
   }
-  printf("Tests run: %d\n", tests_run);
-
+  rtree_destroy(rt);
   return tests_failed != 0;
 }
