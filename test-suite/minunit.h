e82f7e26559a6cd21556ee5835f14f3e89d72571
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-10-29 10:39:08 +0100

    Remove old test suite fluff.

--- test-suite/minunit.h
+++
@@ -1,6 +0,0 @@
-#define mu_assert(message, test) do { if (!(test)) \
-      { printf("[FAIL] %s\n", message); tests_failed++; return 1;}} while (0)
-#define mu_run_test(test) do { test(); tests_run++; } while (0)
-
-extern int tests_run;
-extern int tests_failed;
--- test-suite/test_uniform_frequency.c
+++ test-suite/test_uniform_frequency.c
@@ -25,59 +25,35 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include "rtree.h"
-#include "minunit.h"
-#include "main.h"
+#include "test.h"
 
-#define __GENSYM2(x,y) x##y
-#define __GENSYM1(x,y) __GENSYM2(x,y)
-#define GENSYM(x) __GENSYM1(x,__COUNTER__)
+#define ELEMS 100
+#define SIZE 100000
 
-#define FOR(name, n) for (int name = 0; name < n; name++)
-#define REPEAT(n) FOR(GENSYM(iter), n)
-
-int tests_run = 0;
-int tests_failed = 0;
-
-
-static int test_uniform_frequency() {
+int main() {
   puts("Testing uniform frequency.");
+  int tests_failed = 0;
+
   RTree *rt = rtree_create();
-  const int elems = 100;
-  for (uintptr_t i = 0; i < elems; i++) {
+  for (uintptr_t i = 0; i < ELEMS; i++) {
     rtree_add(rt, (void *) i, 1.0);
   }
-  const int size = 100000;
-  const double prob = 1.0/((double) elems);
-  int *count = calloc(elems, sizeof(int));
-  for (int i = 0; i < size; i++) {
+
+  const double prob = 1.0/((double) ELEMS);
+  int count[ELEMS] = {0};
+  for (int i = 0; i < SIZE; i++) {
     count[(uintptr_t) rtree_rget(rt)]++;
   }
-  for (int i = 0; i < elems; i++) {
-    double freq = ((double) count[i])/size;
+
+  for (int i = 0; i < ELEMS; i++) {
+    double freq = ((double) count[i])/SIZE;
     if (!(prob - 0.005 < freq && freq < prob + 0.005)) {
       printf("[FAIL] Expected frequency for %d to be within %.4f and %.4f, was "
              "%.4f.\n", i, prob - 0.005, prob + 0.005, freq);
-      return 1;
+      tests_failed++;
     }
   }
-  free(count);
+
   rtree_destroy(rt);
-  return 0;
-}
-
-static void all_tests() {
-  mu_run_test(test_uniform_frequency);
-}
-
-int main() {
-  all_tests();
-  if (tests_failed) {
-    printf("%d test%s failed.\n", tests_failed, tests_failed == 1 ? "" : "s");
-  }
-  else {
-    printf("All tests passed.\n");
-  }
-  printf("Tests run: %d\n", tests_run);
-
   return tests_failed != 0;
 }
